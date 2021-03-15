45d51722f3e4a24d343570472049f4f841c85acd
Ross Powell <rosspow49@gmail.com>
2021-03-15 15:23:28 +0000

    Basic minimum viable product for a music player.

--- src/MusicPlayer.py
+++
@@ -1,6 +0,0 @@
-import pygame
-def MusicPlayer(FilePath):
-    pygame.init()
-    pygame.mixer.init()
-    pygame.mixer.music.load(FilePath)
-    pygame.mixer.music.play()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,47 @@
 import pygame
-
 pygame.mixer.init()
-s = pygame.mixer.Sound("bensound-dubstep.wav")
-s.play()
-pygame.time.wait(100000)
+import os
+
+
+def PlaySound(filePath, currentMusic):
+    if not currentMusic == "":
+        currentMusic.stop()
+    pygame.mixer.init()
+    s = pygame.mixer.Sound("../Music/" +filePath)
+    s.play()
+    print("Now playing:",filePath)
+
+    return s
+
+
+
+def GetFileList():
+    files = os.listdir("../Music")
+    return files
+
+def displayFiles(fileList):
+    for entryNumber, entry in enumerate(fileList):
+        print(entryNumber, ":", entry)
+
+def GetFileToPlay(fileList):
+    displayFiles(fileList)
+
+    fileIdentifier = input("Please enter the file name or corresponding number")
+    while fileIdentifier not in fileList  and  int(fileIdentifier) not in range(len(fileList)):
+        print("This is an incorrect identifier")
+        fileIdentifier = input("Please enter the file name or corresponding number")
+
+    if int(fileIdentifier) in range(len(fileList)):
+        fileIdentifier = fileList[int(fileIdentifier)]
+
+    return fileIdentifier
+
+
+def main():
+    soundPlayer = ""
+    musicFiles = GetFileList()
+    while True:
+        fileName = GetFileToPlay(musicFiles)
+        soundPlayer = PlaySound(fileName, soundPlayer)
+
+main()
